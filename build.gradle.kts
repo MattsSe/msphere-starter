294be125cc242335ebb9ea19f3192b5cc8ab7184
matthiasseitz <matthias.seitz@tum.de>
2019-02-10 11:18:13 +0100

    rm ms sdk dependencies for generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.net.URI
-import java.net.URL
-import java.nio.channels.Channels
 
 
 plugins {
@@ -29,27 +24,6 @@ tasks.withType<ShadowJar> {
 
 }
 
-// static endpoint of the mindsphere sdk zip file, may change over time
-val sdkEndpoint =
-    "https://support.industry.siemens.com/dl/files/603/109757603/att_972175/v1/mindsphere-sdk-java-v2.1.0.zip?download=true"
-
-// destination of the downloaded zip file
-val sdkZip = File("./libs/mindsphere-sdk-java.zip")
-
-
-/**
- * find the folder of the unzipped sdk
- * @throws [FileNotFoundException] if no folder `./libs/midsphere-sdk-java-v{x.y.z}` was found
- * @return the [URI] of the maven repo root
- */
-fun localMindSphereRepo(): URI {
-    val files = File("./libs").listFiles { file -> file.name.startsWith("mindsphere-sdk-java") && file.isDirectory }
-    if (files.size != 1) {
-        throw FileNotFoundException("could not find local mindsphere repo `./libs/mindsphere-sdk-java-v{x.y.z}`")
-    }
-    return uri(files[0].toString())
-}
-
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
@@ -65,40 +39,6 @@ dependencies {
     testCompile("junit", "junit", "4.12")
 }
 
-/**
- * download task for the static [sdkEndpoint], if this fails download the sdk file manually
- */
-fun downloadSDK() {
-    if (sdkZip.exists()) {
-        return
-    }
-    val url = URL(sdkEndpoint)
-    val rbc = Channels.newChannel(url.openStream())
-    val fos = FileOutputStream(sdkZip)
-    fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
-}
-
-/**
- * task to unzip a mindsphere sdk file into [sdkZip]
- */
-fun unzip() {
-    if (sdkZip.exists()) {
-        copy {
-            from(zipTree(sdkZip))
-            into("./libs")
-        }
-    } else {
-        val zips =
-            File("./libs").listFiles { file -> file.name.startsWith("mindsphere-sdk-java") && file.endsWith(".zip") }
-        if (zips.size == 1) {
-            copy {
-                from(zipTree(zips[0]))
-                into("./libs")
-            }
-        }
-    }
-}
-
 tasks {
 
     val copyShadow by registering(ShadowJar::class) {
@@ -110,17 +50,7 @@ tasks {
         }
     }
 
-    val installSDK by creating(Copy::class) {
-        unzip()
-    }
-
-    val getSDK by creating(Copy::class) {
-        downloadSDK()
-        finalizedBy(installSDK)
-    }
-
     val build by tasks.existing {
-        unzip()
         dependsOn(copyShadow)
     }
 
